e2bd20b8dad38eec281213097314750fb99970a4
tyhahn <40645362+tylerhahn@users.noreply.github.com>
2022-02-25 11:52:28 -0600

    client

--- client/src/context/Socket.js
+++ client/src/context/Socket.js
@@ -7,7 +7,7 @@ export const SocketProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    const newSocket = io(`http://localhost:8000`);
+    const newSocket = io(`https://mtga-drafter-utdal.ondigitalocean.app/`);
     setSocket(newSocket);
     return () => newSocket.close();
   }, [setSocket]);
